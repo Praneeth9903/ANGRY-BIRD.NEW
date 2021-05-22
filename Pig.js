f05fcded230da1b3019f9d8835682d5707924753
Praneeth9903 <78412027+Praneeth9903@users.noreply.github.com>
2021-05-22 14:53:29 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,39 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+ this.Visiblity=255;
+  }
+display()
+{
+ 
+if((this.body.speed)<2.5)
+{super.display();
+}else
+{
+World.remove(world,this.body)  
+
+
+push();
+this.Visiblity=this.Visiblity-20;
+tint(255,this.Visiblity)
+
+//do nothing
+image(this.image,this.body.position.x,this.body.position.y,50,50)
+pop();
+}}
+score(){
+  if(this.Visiblity<0&&this.Visiblity>-1000){
+score++
+if(score==100){
+  fill("red");
+  text("HORRAY",10,10);
+}
+  }
+
+
+  
+}
+};
+
+
